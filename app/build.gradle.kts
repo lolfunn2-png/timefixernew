cad27c9c03536870fbeb9c4b961d007484081ba7
lolfunn2-png <lolfunn2@gmail.com>
2026-07-01 15:05:51 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
   alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.kapt)
+
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
 }
